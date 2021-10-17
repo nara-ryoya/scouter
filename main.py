39ed58dc9bb7690f002a8d67f874fb37e4ad5d68
nara-ryoya <nara_ryoya@icloud.com>
2021-10-17 11:47:17 +0900

    マルチスレッド化することを考慮して、queueを用いた実装に変更

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 import random
+import threading
+import queue
 
 
 # グローバル領域で各種変数を定義
@@ -26,104 +28,76 @@ height = int(mh / scale)
 width = int(mw / scale)
 mask = np.full((height, width, 3), (70, 110, 0), dtype=np.uint8)
 
+captured_image = queue.Queue()
+processed_image = queue.Queue()
+
 
 #戦闘力（乱数）
 fight = random.randint(5, 530000)
 
+loop_flg = True
 
-def init():
-    glutInitWindowPosition(0, 0)
-    #glutInitWindowSizeは(w, h)の順番
-    # glutInitWindowSize(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    glutInitWindowSize(width, height)
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
-    glutCreateWindow("Scouter")
+def main():
 
-def detect_people(curFrame):
-    gray = cv2.cvtColor(curFrame, cv2.COLOR_BGRA2GRAY)
-    faceRect = cascade.detectMultiScale(gray)
-    if len(faceRect) > 0:
-        for fx, fy, fw, fh in faceRect:
-            #顔にモザイク
+    while loop_flg:
+        capture()
+        detect_person_and_draw()
+        k = cv2.waitKey(10)
+        #Escキー or'q' を押されたら終了
+        if k == 27 or k == ord('q'):
+            break
+    cap.release()
 
-            #戦闘力表示
-            textColor = (0, 244, 243)
-            fontSize = 64
-            font = ImageFont.truetype('SFNSMonoItalic.ttf', fontSize)
-            img_pil = Image.fromarray(curFrame)
-            draw = ImageDraw.Draw(img_pil)
-            x = fx + (fw / 2) - (font.getsize('FIGHT')[0] / 2)
-            y = fy - (fontSize * 1.5) + 5
-            draw.text((x, y), 'FIGHT', font = font, fill = textColor)
-            x = fx + (fw / 2) - (font.getsize(str(fight))[0] / 2)
-            y = fy - (fontSize * 0.5)
-            draw.text((x, y), str(fight), font = font, fill = textColor)
-            curFrame = np.array(img_pil)
+def capture():
+    ret, frame = cap.read()
+    if not ret:
+        global loop_flg
+        loop_flg = False
+    captured_image.put(frame)
 
-            #真ん中三角
-            p1 = (int(fx + (fw / 2)), int(fy + (fh * 1.0)))
-            p2 = (int(fx + (fw / 2) - 40), int(fy + (fh * 1.0) + 60))
-            p3 = (int(fx + (fw / 2) + 40), int(fy + (fh * 1.0) + 60))
-            triangle = np.array([p1, p2, p3])
-            cv2.drawContours(curFrame, [triangle], 0, textColor, -1)
+def detect_person_and_draw():
+    if captured_image:
+        frame = captured_image.get()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
+        faceRect = cascade.detectMultiScale(gray)
+        frame = cv2.addWeighted(frame, 0.8, mask, 0.9, 0)
+        if len(faceRect) > 0:
+            for fx, fy, fw, fh in faceRect:
+                textColor = (0, 244, 243)
+                fontSize = 64
+                font = ImageFont.FreeTypeFont('DSEG7Modern-Light.ttf', fontSize)
+                img_pil = Image.fromarray(frame)
+                draw = ImageDraw.Draw(img_pil)
+                x = fx + (fw / 2) - (font.getsize('FIGHT')[0] / 2)
+                y = fy - (fontSize * 1.5)
+                draw.text((x, y - 30), 'FIGHT', font = font, fill = textColor)
+                x = fx + (fw / 2) - (font.getsize(str(fight))[0] / 2)
+                y = fy - (fontSize * 0.5)
+                draw.text((x, y), str(fight), font = font, fill = textColor)
+                frame = np.array(img_pil)
 
-            #左三角
-            p1 = (int(fx - 35)), int(fy + (fh * 0.5) + 30)
-            p2 = (int(fx - 95), int(fy + (fh * 0.5)))
-            p3 = (int(fx - 95), int(fy + (fh * 0.5) + 60))
-            triangle = np.array([p1, p2, p3])
-            cv2.drawContours(curFrame, [triangle], 0, textColor, -1)
+                #真ん中三角
+                p1 = (int(fx + (fw / 2)), int(fy + (fh * 1.0)))
+                p2 = (int(fx + (fw / 2) - 40), int(fy + (fh * 1.0) + 60))
+                p3 = (int(fx + (fw / 2) + 40), int(fy + (fh * 1.0) + 60))
+                triangle = np.array([p1, p2, p3])
+                cv2.drawContours(frame, [triangle], 0, textColor, -1)
 
-            #右三角
-            p1 = (int(fx + fw + 35)), int(fy + (fh * 0.5) + 30)
-            p2 = (int(fx + fw + 95), int(fy + (fh * 0.5)))
-            p3 = (int(fx + fw + 95), int(fy + (fh * 0.5) + 60))
-            triangle = np.array([p1, p2, p3])
-            cv2.drawContours(curFrame, [triangle], 0, textColor, -1)
-    return curFrame
+                #左三角
+                p1 = (int(fx - 35)), int(fy + (fh * 0.5) + 30)
+                p2 = (int(fx - 95), int(fy + (fh * 0.5)))
+                p3 = (int(fx - 95), int(fy + (fh * 0.5) + 60))
+                triangle = np.array([p1, p2, p3])
+                cv2.drawContours(frame, [triangle], 0, textColor, -1)
 
-def draw():
-    _, curFrame = cap.read()
+                #右三角
+                p1 = (int(fx + fw + 35)), int(fy + (fh * 0.5) + 30)
+                p2 = (int(fx + fw + 95), int(fy + (fh * 0.5)))
+                p3 = (int(fx + fw + 95), int(fy + (fh * 0.5) + 60))
+                triangle = np.array([p1, p2, p3])
+                cv2.drawContours(frame, [triangle], 0, textColor, -1)
 
-    curFrame = detect_people(curFrame)
-    curFrame = cv2.addWeighted(curFrame, 0.8, mask , 0.9, 0)
-
-    #フレーム描画
-    curFrame = cv2.cvtColor(curFrame, cv2.COLOR_BGR2RGB)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, curFrame.shape[1], curFrame.shape[0], 0, GL_RGB, GL_UNSIGNED_BYTE, curFrame)
-
-    glEnable(GL_TEXTURE_2D)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-
-    glBegin(GL_QUADS) 
-    glTexCoord2d(0.0, 1.0)
-    glVertex3d(-1.0, -1.0, 0.0)
-    glTexCoord2d(1.0, 1.0)
-    glVertex3d(1.0, -1.0, 0.0)
-    glTexCoord2d(1.0, 0.0)
-    glVertex3d(1.0,  1.0, 0.0)
-    glTexCoord2d(0.0, 0.0)
-    glVertex3d(-1.0, 1.0, 0.0)
-    glEnd()
-
-    glFlush()
-    glutSwapBuffers()
-
-def idle():
-    glutPostRedisplay()
-
-def keyboard(key, x, y):
-    key = key.decode('utf-8')
-    if key == 'q':
-        exit()
+        cv2.imshow("scouter", frame)
 
 if __name__ == "__main__":
-    init()
-    glutDisplayFunc(draw)
-    glutKeyboardFunc(keyboard)
-    glutIdleFunc(idle)
-    glutMainLoop()
-
-    cap.release()
+    main()
